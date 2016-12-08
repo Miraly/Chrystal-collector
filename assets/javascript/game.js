b87f31cf4e9d030a8759792ab51b7d5f4c371d37
Ksenia D <Ksenia D>
2016-12-08 11:38:34 -0500

    almost done ...working on sum function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,41 @@
+	var number;
+	var gem1;
+	var gem2;
+	var gem3;
+	var gem4;
+	var total;
 
+	$(document).ready(function() {
+		number = Math.floor((Math.random() * 101) + 19);
+		$('#givenNumber').text(number);
+		
+		gem1 = Math.floor((Math.random() * 11) + 1);
+		
+		gem2 = Math.floor((Math.random() * 11) + 1);
+		if (gem1 === gem2) {
+		gem2 = Math.floor((Math.random() * 11) + 1);
+		}
+		
+		gem3 = Math.floor((Math.random() * 11) + 1);
+		if (gem1 === gem3 || gem2 === gem3 ) {
+		gem3 = Math.floor((Math.random() * 11) + 1);
+		}
+						
+		gem4 = Math.floor((Math.random() * 11) + 1);
+		if (gem1 === gem4 || gem2 === gem4 || gem3 === gem4 ) {
+		gem4 = Math.floor((Math.random() * 11) + 1);
+		}
+		
+		$("#gem1").val(gem1);
+		$("#gem2").val(gem2);
+		$("#gem3").val(gem3);
+		$("#gem4").val(gem4);
+		
+		$("button").click(function() {
+			console.log(this);
+			total = parseInt(total) + parseInt($(this).val());  
+			$("p").html(parseInt(total) + parseInt($(this).val()));
+		});
+		
+
+	});
